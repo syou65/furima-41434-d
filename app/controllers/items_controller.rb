702495100f6a9f553e37c11db22e10a10f853d6b
syou65 <tokyonihon0305@gmail.com>
2025-01-13 18:06:38 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
  before_action :authenticate_user!, only: [:new, :create,:edit, :destroy]
  before_action :set_item, only: [:edit, :show, :update, :destroy]
+ before_action :check_owner, only: [:edit, :destroy]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -18,14 +19,10 @@ class ItemsController < ApplicationController
   def show
   end
   def edit
-    redirect_to root_path unless current_user == @item.user 
   end
   def update
-    if @item.update(item_params)
-      redirect_to item_path(@item), notice: 
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    @item.destroy
+    redirect_to root_path
   end
   def destroy
     if @item.destroy
